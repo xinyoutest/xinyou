fc7b02f207d924dd14f60fc7770d76288db2a497
xugit <625753284@qq.com>
2015-07-13 15:47:50 +0800

    修复一些问题

--- apps/all_tests_old.py
+++ apps/all_tests_old.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 
 import unittest
 import time
@@ -8,67 +8,71 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 import re
-sys.path.append(os.path.dirname(__file__)+'\\action')
+sys.path.append(os.path.dirname(__file__) + '\\action')
 from object_db import *
 
 
 def test(action_path):
-	case_list=os.listdir(action_path)
-	pattern = re.compile('^(start_){1}\w*.py$')
-	for dirname in case_list:
-		page_dir=action_path+"\\"+dirname
-		if os.path.isdir(page_dir):
-			test_dir=[]
-			case_s=os.listdir(page_dir)
-			for case in case_s:
-				match=pattern.match(case)
-				if match:
-					test_dir.append(case)
-			createSuiteList(page_dir)
-				
-# #筛选测试用例文件，并创建测试套件
-def createSuiteList(action_path,report_name,top_level_dir):
-	case_dir = []
-	case_list = os.listdir(action_path)
-	pattern = re.compile('^(start_){1}\w*.py$')
-	for case in case_list:
-		match = pattern.match(case)
-		if match:
-			case_dir.append(case)
-	for n in case_dir:
-		testunit = unittest.TestSuite()
-		#discover方法定义
-		discover = unittest.defaultTestLoader.discover(action_path,
-			pattern = 'start_*.py',
-			top_level_dir= top_level_dir)
-		#discover方法筛选出来的用例，循环添加到测试套件中
-		for test_suite in discover:
-			for test_case in test_suite:
-				testunit.addTests(test_case)
-				# sys.exit()
-	return testunit, case_dir,report_name
+    case_list = os.listdir(action_path)
+    pattern = re.compile('^(start_){1}\w*.py$')
+    for dirname in case_list:
+        page_dir = action_path + "\\" + dirname
+        if os.path.isdir(page_dir):
+            test_dir = []
+            case_s = os.listdir(page_dir)
+            for case in case_s:
+                match = pattern.match(case)
+                if match:
+                    test_dir.append(case)
+            createSuiteList(page_dir)
 
-#执行测试套件并生成测试报告
-def startRunner(suite, case_dir, report_path,report_name):
-	#取当前时间：年月日时分秒
-	# now = time.strftime("%Y%m%d.%H%M%S", time.localtime(time.time()))
-	#定义个报告存放路径，支持相对路径。
-	filename = report_path+ '\\'+ str(report_name)+ '_result.html'
-	fp = file(filename, 'wb')
-	for item in suite:
-		runner = HTMLTestRunner.HTMLTestRunner(
-			stream = fp,
-			title = str(item) + u'测试报告',
-			description = u'测试用例执行情况：')
-		runner.run(item)
+# 筛选测试用例文件，并创建测试套件
+
+
+def createSuiteList(action_path, report_name, top_level_dir):
+    case_dir = []
+    case_list = os.listdir(action_path)
+    pattern = re.compile('^(start_){1}\w*.py$')
+    for case in case_list:
+        match = pattern.match(case)
+        if match:
+            case_dir.append(case)
+    for n in case_dir:
+        testunit = unittest.TestSuite()
+        # discover方法定义
+        discover = unittest.defaultTestLoader.discover(action_path,
+                                                       pattern='start_*.py',
+                                                       top_level_dir=top_level_dir)
+        # discover方法筛选出来的用例，循环添加到测试套件中
+        for test_suite in discover:
+            for test_case in test_suite:
+                testunit.addTests(test_case)
+                # sys.exit()
+    return testunit, case_dir, report_name
+
+# 执行测试套件并生成测试报告
+
+
+def startRunner(suite, case_dir, report_path, report_name):
+    # 取当前时间：年月日时分秒
+    # now = time.strftime("%Y%m%d.%H%M%S", time.localtime(time.time()))
+    # 定义个报告存放路径，支持相对路径。
+    filename = report_path + '\\' + str(report_name) + '_result.html'
+    fp = file(filename, 'wb')
+    for item in suite:
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=fp,
+            title=str(item) + u'测试报告',
+            description=u'测试用例执行情况：')
+        runner.run(item)
 
 
 if __name__ == "__main__":
-	case_list=os.listdir(action_path)
-	for dirname in case_list:
-		page_dir=action_path+"\\"+dirname
-		if os.path.isdir(page_dir):
-			report_name=os.path.basename(page_dir)
-			top_level_dir=os.path.dirname(page_dir)
-			runtmp = createSuiteList(page_dir,report_name,top_level_dir)
-			startRunner(runtmp[0], runtmp[1], report_path,report_name)
+    case_list = os.listdir(action_path)
+    for dirname in case_list:
+        page_dir = action_path + "\\" + dirname
+        if os.path.isdir(page_dir):
+            report_name = os.path.basename(page_dir)
+            top_level_dir = os.path.dirname(page_dir)
+            runtmp = createSuiteList(page_dir, report_name, top_level_dir)
+            startRunner(runtmp[0], runtmp[1], report_path, report_name)
---
+++ apps/main.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+
+import unittest
+import HTMLTestRunner
+import logging
+import logging.config
+import os
+import time
+import sys
+import re
+apps_path = os.path.dirname(__file__)
+action_path = apps_path + "\\" + "action"
+conf_path = apps_path + "\\" + "conf"
+sys.path.append(action_path)
+report_path = apps_path + "\\" + "report"
+# 筛选测试用例文件，并创建测试套件
+
+
+def createSuiteList(subpath, childpath):
+    case_lists = []  # 目录下有效测试用例列表
+    flists = os.listdir(childpath)  # 目录下所有文件列表
+    # main_logger.info(childpath + "目录下所有文件列表为：" + str(flists))
+    pattern = re.compile('^(start_){1}\w*.py$')
+    for fname in flists:
+        match = pattern.match(fname)
+        if match:
+            case_lists.append(fname)
+    # main_logger.info(childpath + "目录下有效测试用例文件列表为：" + str(case_lists))
+    suite_lists = []
+    for case in case_lists:
+        test_unit = unittest.TestSuite()
+        # discover方法定义
+        discovers = unittest.defaultTestLoader.discover(
+            childpath, pattern='start_*.py', top_level_dir=subpath)
+        # discover方法筛选出来的用例，循环添加到测试套件中
+        for test_suite in discovers:
+            test_unit.addTests(test_suite)
+    suite_lists.append(test_unit)
+    # main_logger.info(childpath + "目录下筛选出的测试套件列表为：" + str(suite_lists))
+    return suite_lists, case_lists
+
+# 执行测试套件并生成测试报告
+
+
+def startRunner(suite_lists, case_lists, report_path):
+    # 取当前时间：年月日时分秒
+    now = time.strftime("%Y%m%d.%H%M%S", time.localtime(time.time()))
+    # 定义个报告存放路径，支持相对路径。
+    filename = report_path + '\\' + now + 'result.html'
+    fp = file(filename, 'wb')
+    for index, item in enumerate(suite_lists):
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=fp,
+            title=str(case_lists[index]) + u'测试报告',
+            description=u'测试用例执行情况：')
+        runner.run(item)
+
+
+if __name__ == "__main__":
+    # config_log = conf_path + "\\" + "logging.conf"
+    # logging.config.fileConfig(config_log)
+    # main_logger = logging.getLogger('main')
+    subpath = action_path
+    childpath = subpath + "\\" + "login"
+    suite_lists, case_lists = createSuiteList(subpath, childpath)
+    startRunner(suite_lists, case_lists, report_path)
